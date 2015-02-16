fd390a1831130be84ee9841224c6355f63fc3417
Joshua Duffy <mail@joshuaduffy.org>
2015-02-16 22:54:37 +0000

    Simplified responses

--- dota2api/src/response.py
+++ dota2api/src/response.py
@@ -3,18 +3,14 @@
 import parse
 
 class Dota2Response(object):
-    """Generic response used when we don't parse"""
+    """Generic response"""
     def __init__(self, response, url):
         self.url = url
         self.resp = response
 
-class Dota2MatchDetails(object):
-    """This is used to package up the response"""
+class Dota2MatchDetails(Dota2Response):
+    """Match details"""
     def __init__(self, response, url):
-        self.url = url
-        self.resp = response
         if 'players' in self.resp:
             self.resp = parse.hero_id(self.resp)
             self.resp = parse.item_id(self.resp)
-        else:
-            self.resp = response
