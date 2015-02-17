c294677c572b9a9e66fa57b6ed85fb2d45c81d3b
Joshua Duffy <mail@joshuaduffy.org>
2015-02-17 14:16:12 +0000

    Fixed the use of resp instead of dict

--- tests/api_test.py
+++ tests/api_test.py
@@ -15,17 +15,17 @@ class ApiMatchTests(unittest.TestCase):
     def get_match_history_test(self):
         """Test get_match_history"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_match_history().resp), type(dict()))
+        self.assertEqual(type(self.api_test.get_match_history().dict), type(dict()))
         # Do we default at 100 responses
         self.assertEquals(
-            len(self.api_test.get_match_history().resp['matches']), 100)
+            len(self.api_test.get_match_history().dict['matches']), 100)
         # Can we filter that to 1 response
         self.assertEquals(len(self.api_test.get_match_history(
-            matches_requested=1).resp['matches']), 1)
+            matches_requested=1).dict['matches']), 1)
         # Can we use the parameter to get only 1 players matches
         for matches in self.api_test \
             .get_match_history(account_id=88585077,
-                               matches_requested=10).resp['matches']:
+                               matches_requested=10).dict['matches']:
             if 19672354 in matches['players']:
                 self.assertTrue(True)
 
@@ -33,9 +33,9 @@ class ApiMatchTests(unittest.TestCase):
         """Test get_match_details"""
         # Is the response a dictionary
         self.assertEqual(type(self.api_test.get_match_details(
-            match_id=988604774).resp), type(dict()))
+            match_id=988604774).dict), type(dict()))
         # Do we get an error with no match ID
-        self.assertEquals(self.api_test.get_match_details().resp['error'],
+        self.assertEquals(self.api_test.get_match_details().dict['error'],
                           "No Match ID specified")
 
 class ApiOtherTests(unittest.TestCase):
@@ -47,43 +47,43 @@ class ApiOtherTests(unittest.TestCase):
     def get_league_listing_test(self):
         """Test get_league_listing"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_league_listing().resp),
+        self.assertEqual(type(self.api_test.get_league_listing().dict),
                       type(dict()))
 
     def get_live_league_games_test(self):
         """Test get_live_league_games"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_live_league_games().resp),
+        self.assertEqual(type(self.api_test.get_live_league_games().dict),
                       type(dict()))
 
     def get_team_info_by_team_id_test(self):
         """Test get_team_info_by_team_id"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_team_info_by_team_id().resp),
+        self.assertEqual(type(self.api_test.get_team_info_by_team_id().dict),
                       type(dict()))
 
     def get_player_summaries_test(self):
         """Test get_player_summaries"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_player_summaries().resp),
+        self.assertEqual(type(self.api_test.get_player_summaries().dict),
                       type(dict()))
 
     def get_heroes_test(self):
         """Test get_heroes"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_heroes().resp),
+        self.assertEqual(type(self.api_test.get_heroes().dict),
                       type(dict()))
 
     def get_game_items_test(self):
         """Test get_game_items"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_game_items().resp),
+        self.assertEqual(type(self.api_test.get_game_items().dict),
                       type(dict()))
 
     def get_tournament_prize_pool_test(self):
         """Test get_tournament_prize_pool"""
         # Is the response a dictionary
-        self.assertEqual(type(self.api_test.get_tournament_prize_pool().resp),
+        self.assertEqual(type(self.api_test.get_tournament_prize_pool().dict),
                       type(dict()))
 
 def invalid_api_key_test():
