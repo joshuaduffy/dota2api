a05d76f86bc64baf74991426eaaab66de809383f
Joshua Duffy <mail@joshuaduffy.org>
2015-03-18 23:00:23 +0000

    Check key exists

--- dota2api/src/response.py
+++ dota2api/src/response.py
@@ -16,8 +16,9 @@ def build(req, url):
     if 'result' in req_resp:
         if 'error' in req_resp['result']:
             raise APIError(req_resp['result']['error'])
-        if req_resp['result']['status'] != 1:
-            raise APIError(req_resp['result']['statusDetail'])
+        if 'status' in req_resp['result']:
+            if req_resp['result']['status'] != 1:
+                raise APIError(req_resp['result']['statusDetail'])
         resp = Dota2Dict(req_resp['result'])
     elif 'response' in req_resp:
         resp = Dota2Dict(req_resp['response'])
