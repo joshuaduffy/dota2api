7024914ffbe137647a683cb3066dca4638d93781
Joshua Duffy <mail@joshuaduffy.org>
2015-03-18 21:29:17 +0000

    Updated documentation

--- dota2api/src/exceptions.py
+++ dota2api/src/exceptions.py
@@ -9,18 +9,7 @@ class BaseError(Exception):
 
 class APIError(BaseError):
     """
-    Raised when the API response is an error
-    """
-    def __init__(self, msg):
-        self.msg = msg
-
-    def __str__(self):
-        return repr(self.msg)
-
-
-class APIStatusError:
-    """
-    Raised when the status does not equal one
+    Raised when the API response is an error, or the status does not equal one
     """
     def __init__(self, msg):
         self.msg = msg
