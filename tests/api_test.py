f8bc9e96a4a6f7877dcfdd771ea6e3029916554e
Joshua Duffy <mail@joshuaduffy.org>
2015-03-24 22:26:18 +0000

    Improved tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -11,40 +11,31 @@ from utils import RequestMock
 
 
 class APITest(unittest.TestCase):
+    def setUp(self):
+        self.api = dota2api.Initialise()
+
     def no_environment_variable_set_test(self):
         # Pass the API key as a positional argument
         self.api_key = os.environ['D2_API_KEY']
-        self.api = dota2api.Initialise(self.api_key)
-        match = self.api.get_match_details(match_id=988604774)
-
+        api = dota2api.Initialise(self.api_key)
+        match = api.get_match_details(match_id=988604774)
         # Do we get a match back
         mock = RequestMock()
         stored_match = mock.configure_single_match_result().json_result
-
         # Check the response is the same as the stored one
         self.assertEqual(stored_match['result']['match_id'], match['match_id'])
-
         # Check it is our custom dict type
         self.assertEqual(type(Dota2Dict()), type(match))
 
-        # Test json function loads json
-        try:
-            json.loads(match.json)
-        except ValueError:
-            self.fail("JSON does not load!")
+    def json_loads_test(self):
+            # Test json function loads json
+            match = self.api.get_match_details(match_id=988604774)
+            try:
+                json.loads(match.json)
+            except ValueError:
+                self.fail("JSON does not load!")
 
-        # Test the wrong API key
-        with self.assertRaises(APIAuthenticationError):
-            dota2api.Initialise("sdfsdfsdf").get_match_history()
-
-    def get_match_history_no_kwargs(self):
-        # Don't use kwargs when calling
-        assert True
-
-    def get_match_details_no_kwargs(self):
-        # Don't use kwargs when calling
-        assert True
-
-    def test_logging(self):
-        # Something to cover the logging
-        assert True
+    def wrong_api_key_test(self):
+            # Test the wrong API key
+            with self.assertRaises(APIAuthenticationError):
+                dota2api.Initialise("sdfsdfsdf").get_match_history()
